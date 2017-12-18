77881b1854f57369fe70ac8236c58abfeeef3561
ArunOnCloud <arun91iitr@gmail.com>
2017-12-18 10:39:48 +0530

    commmit java 8 code

--- CodeExample/src/javaEight/ErasureConcept.java
+++ CodeExample/src/javaEight/ErasureConcept.java
@@ -1,4 +1,4 @@
-package ConceptImplementation;
+package javaEight;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,16 +9,21 @@ public class ErasureConcept {
 		ArrayList<String> al = new ArrayList<String>();
 		al.add("a");
 		al.add("b");
- 
+		System.out.println("println");
+       for(int i=0;i<al.size();i++)
+       {
+    	  String s=al.get(i);
+    	  System.out.println(s);
+       }
 		//accept(al);// it
 		accept(al);// it
 	}
-// //below implementation with line 13 give compilation error because erasure is implementated at compilation level
+ //below implementation with line 13 give compilation error because erasure is implementated at compilation level
 	
-/*	public static void accept(ArrayList<Object> al){
-		for(Object o: al)
-			System.out.println(o);
-	}*/
+//	public static void accept(ArrayList<Object> al){
+//		for(Object o: al)
+//			System.out.println(o);
+//	}
 	public static void accept(ArrayList<? extends Object> al){
 		for(Object o: al)
 			System.out.println(o);
