8a686ebb733a198b69fb2a3ddebed83c47aa1361
Arun Kumar Ram <arun.ram@freecharge.com>
2018-07-22 20:55:09 +0530

    new code

--- CodeExample/src/Hackerearth/HackerearthSamsung.java
+++ CodeExample/src/Hackerearth/HackerearthSamsung.java
@@ -3,7 +3,6 @@ package Hackerearth;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
